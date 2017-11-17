107e89cfe0d09343f84bc8f722f5d10929eeabf6
Niki Card <agenttat@gmail.com>
2017-11-16 17:32:39 -0800

    basic functionality completed!
    this involves a large amount of switches, variable, and common events.
    random events still need to be implemented, but travelling which takes
    multiple days is currently working perfectly.

--- Lunatic_Nautilus/Scripts/FFVII_Menu.rb
+++ Lunatic_Nautilus/Scripts/FFVII_Menu.rb
@@ -30,7 +30,18 @@ class Scene_Menu
      s4 = "Status"
      s5 = "Save"
      s6 = "End Game"
+     
+     s7 = "Progress day" #new!!
+     
+   #if ship is not docked (2) and ship can be used (3)
+   if $game_switches[2] == false and $game_switches[3] == true and $game_map.map_id == 1
+    #change this depending on current ship
+     
+     @command_window = Window_Command.new(160, [s1, s2, s3, s4, s5, s6, s7])
+   else
      @command_window = Window_Command.new(160, [s1, s2, s3, s4, s5, s6])
+   end
+   
      @command_window.index = @menu_index
      @command_window.x = 482
      @command_window.y = 0
@@ -172,6 +183,13 @@ class Scene_Menu
            $game_system.se_play($data_system.decision_se)
            # Switch to end game screen
            $scene = Scene_End.new
+        when 6   # progress day
+           # Play decision SE
+           $game_system.se_play($data_system.decision_se)
+           # exit back to map
+           $scene = Scene_Map.new
+           $game_temp.common_event_id = 3 #[3]
+           #activate common event
         end
         return
      end
