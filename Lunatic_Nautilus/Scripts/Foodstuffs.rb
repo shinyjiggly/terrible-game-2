233a28141a1b13b25291d470fd35c338ad5544ad
Niki Card <agenttat@gmail.com>
2018-02-09 01:52:52 -0800

    Frostlands initial commit
    -added snowfall
    -added lighthouse tileset
    -added a bunch of .it files
    -turned down the [B]
    -added some items to the database
    -layed down some psuedocode to mess with later in the scripts
    -replaced the templar class with the time cook class (name pending)
    
    Notes: I figure we can add our stuff to the same project and then split
    them later so we don't wind up being stuck with depriciated stuff on
    either end, considering they're both supposed to be prologue type
    stories to the same main story, and we'll need a lot of these resources
    for the main game when we're ready to work on that too.

---
+++ Lunatic_Nautilus/Scripts/Foodstuffs.rb
@@ -0,0 +1,15 @@
+#---------------------------------
+# Foodstuffs script
+# By shinyjiggly
+#---------------------------------
+
+#if item [a] is used,
+
+#(set variable AAAAA to ID of actor item was used on)
+#(AAAAA = $game_actors[id])
+
+#if actor [1] is the target (if AAAAA == [1]),
+#then cause effect [A]
+
+#elsif actor [2] is the target,
+#then cause effect [B]
